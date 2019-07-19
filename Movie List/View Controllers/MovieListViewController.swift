c9a780725a20501f8262d701f12739147510da8c
Jake Connerly <jlconnerly@gmail.com>
2019-07-19 12:07:19 -0500

    Stretch Goal, 'Swipe to Delete' Complete

--- Movie List/View Controllers/MovieListViewController.swift
+++ Movie List/View Controllers/MovieListViewController.swift
@@ -64,6 +64,13 @@ extension MovieListViewController: UITableViewDataSource {
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            movieList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
 
 extension MovieListViewController: UITableViewDelegate {
